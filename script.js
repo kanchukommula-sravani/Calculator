185a8ab4e1ebe6c332b85ada401eab5046fdbade
kanchukommula-sravani <kanchukommulasravani858@gmail.com>
2025-05-15 10:27:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    const a = parseFloat(document.getElementById('a').value);
+    const b = parseFloat(document.getElementById('b').value);
+    document.getElementById('result').textContent = a + b;
+  }
+  
